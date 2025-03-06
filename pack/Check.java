9d6c28588b46666c5762c864ba2cd0a062075a95
Lenovo-PC <dana.zhappar@mail.ru>
2025-03-06 18:35:06 +0600

    3 hw

--- pack/Check.java
+++ pack/Check.java
@@ -3,5 +3,6 @@ package pack;
 public class  Check{
      public void checkClassLoader(){
          System.out.println("ClassLoader is working!");
+         System.out.println("Using loader: " + this.getClass().getClassLoader());
      }
  }
